3876acc94ad346749af3dd6fc35c7e68ad5994de
Facundo Sura <facundomesura@gmail.com>
2024-09-02 18:47:40 -0300

    landing page

--- client/src/app/Landing.tsx
+++ client/src/app/Landing.tsx
@@ -3,18 +3,18 @@ import React, { useEffect, useState } from 'react';
 
 const Landing: React.FC = () => {
   
-  const [showText, setShowText] = useState(false);
+  const [animate, setAnimate] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
-      setShowText(true);
-    }, 500); // Retraso de 0.5 segundos antes de iniciar la transición
+      setAnimate(true);
+    }, 500); // Retraso de 0.5 segundos antes de iniciar la animación
   }, []);
 
   return (
     <div className="relative w-full h-screen overflow-hidden">
       <video 
-        className="absolute inset-0 w-full h-full object-cover" 
+        className={`absolute inset-0 w-full h-full object-cover transition-transform duration-1000 ${animate ? 'translate-y-0' : '-translate-y-full'}`} 
         autoPlay 
         loop 
         muted
@@ -22,7 +22,7 @@ const Landing: React.FC = () => {
         <source src="/videos/landing.mp4" type="video/mp4" />
         Tu navegador no soporta la etiqueta de video.
       </video>
-      <div className={`relative z-10 flex flex-col items-center justify-center h-full text-white text-center px-4 transition-opacity duration-1000 ${showText ? 'opacity-100' : 'opacity-0'}`}>
+      <div className={`relative z-10 flex flex-col items-center justify-center h-full text-white text-center px-4  duration-1000 ${animate ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-full'}`}>
         <h1 className="text-4xl md:text-6xl font-bold">Estudio Jurídico Sura</h1>
         <p className="text-lg md:text-2xl mt-4">Tu mejor defensa en cualquier situación</p>
       </div>
