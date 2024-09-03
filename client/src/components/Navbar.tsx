d8db1eafcdb0307d8be5d524cbe3fb46da435bf8
Facundo Sura <facundomesura@gmail.com>
2024-09-02 23:02:47 -0300

    links de la navbar + pagina de consulta creada pero no desarrollada

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,17 +1,37 @@
 "use client";
+import Link from "next/link";
 import Image from "next/image";
 import logo from "../../public/imagenes/icono.png";
+import { pages } from "next/dist/build/templates/app-page";
 
 export default function Navbar() {
   return (
     <nav className="bg-white text-black flex flex-row justify-evenly justify-items-center items-center">
-      <Image src={logo} alt="logo" className="m-1 p-1 h-36 w-36" />
-      <h1 className="m-1 p-1 text-3xl font-bold">Estudio Juridico Sura </h1>
+      <Link href="/">
+        <Image src={logo} alt="logo" className="m-1 p-1 h-36 w-36" />
+        <h1 className="m-1 p-1 text-3xl font-bold">Estudio Juridico Sura </h1>
+      </Link>
       <menu>
-        <button className="hover:bg-gradient-to-r from-fuchsia-700 via-red-500 to-orange-600 hover:text-white rounded-md m-1 p-1 w-36 h-14">NOSOTROS</button>
-        <button className="hover:bg-gradient-to-r from-fuchsia-700 via-red-500 to-orange-600 hover:text-white rounded-md m-1 p-1 w-36 h-14">SERVICIOS</button>
-        <button className="hover:bg-gradient-to-r from-fuchsia-700 via-red-500 to-orange-600 hover:text-white rounded-md m-1 p-1 w-36 h-14">AGENDA</button>
-        <button className="hover:bg-gradient-to-r from-fuchsia-700 via-red-500 to-orange-600 hover:text-white rounded-md m-1 p-1 w-36 h-14">CONSULTA</button>
+        <Link href="/nosotros">
+          <button className="hover:bg-gradient-to-r from-fuchsia-700 via-red-500 to-orange-600 hover:text-white rounded-md m-1 p-1 w-36 h-14">
+            NOSOTROS
+          </button>
+        </Link>
+        <Link href="/servicios">
+          <button className="hover:bg-gradient-to-r from-fuchsia-700 via-red-500 to-orange-600 hover:text-white rounded-md m-1 p-1 w-36 h-14">
+            SERVICIOS
+          </button>
+        </Link>
+        <Link href="/agenda">
+          <button className="hover:bg-gradient-to-r from-fuchsia-700 via-red-500 to-orange-600 hover:text-white rounded-md m-1 p-1 w-36 h-14">
+            AGENDA
+          </button>
+        </Link>
+        <Link href="/consulta">
+          <button className="hover:bg-gradient-to-r from-fuchsia-700 via-red-500 to-orange-600 hover:text-white rounded-md m-1 p-1 w-36 h-14">
+            CONSULTA
+          </button>
+        </Link>
       </menu>
     </nav>
   );
