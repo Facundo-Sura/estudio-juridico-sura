e3744841abeec7fbeb4669b0c695492be2956411
Facundo Sura <facundomesura@gmail.com>
2024-09-11 20:09:15 -0300

    página de nosotros lista, falta agregar el resto del equipo, página de servicios lista, falta agregar los demas items a las demas listas

--- client/src/app/nosotros/page.tsx
+++ client/src/app/nosotros/page.tsx
@@ -7,9 +7,11 @@ export default function Nosotros() {
   return (
     <div>
       <Navbar />
-      <div className="bg-white text-black text-center p-24">
-        <h1 className="m-14">Nosotros</h1>
-        <p>
+      <div className="bg-white text-black">
+        <div className="bg-[url('/imagenes/fontpage.jpg')] bg-cover bg-center h-full w-full my-20 py-44">
+          <h1 className="text-4xl text-white text-center">Nosotros</h1>
+        </div>
+        <p className="px-44">
           Somos un estudio jurídico donde el ejercicio del derecho no es sólo
           una profesión, sino una vocación. Abordamos cada caso garantizando un
           enfoque integrado y preventivo, con el compromiso y la responsabilidad
@@ -18,10 +20,10 @@ export default function Nosotros() {
           fundamentales de las personas y la visión de una sociedad más justa y
           equitativa.
         </p>
-        <div className="p-12 flex justify-center ">
+        <div className="p-20 flex justify-center ">
           <Image src={SGM} alt="Sura Norma Gladys" />
           <div className="m-8 p-8">
-            <h1 className="m-8">Gladys sura</h1>
+            <h1 className="m-8 text-2xl font-bold">Gladys sura</h1>
             <h6 className="m-8">
               MP 1-37569 / MAT. FED. Tº 506 Fº 962 / MAT. MED. 876
             </h6>
--- client/src/app/servicios/page.tsx
+++ client/src/app/servicios/page.tsx
@@ -1,33 +1,116 @@
-import Navbar from "@/components/Navbar";
+"use client";
 import React from "react";
+import { useState } from "react";
+import Navbar from "@/components/Navbar";
 
 export default function Servicioe() {
+  // Estado que almacena el índice de la lista activa
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+  // Función para alternar la lista activa
+  const toggleList = (index: number) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
+  const sections = [
+    { title: "+ Administrativo", content: [] }, // No tiene contenido
+    {
+      title: "+ Civil",
+      content: [
+        "Procesos Sucesorios.",
+        "Declaratorias de herederos.",
+        "Adjudicación y venta de bienes.",
+        "Desalojos.",
+        "Cobro de alquileres.",
+        "División de condominios.",
+        "Accidentes de tránsito.",
+        "Malas Praxis.",
+        "Incumplimientos contractuales.",
+        "Ejecución de contratos.",
+        "Juicios de Escrituración. Usucapiones.",
+        "Demandas en relaciones de consumo.",
+        "Constitución de sociedades comerciales y civiles.",
+        "Ejecución de acuerdos de mediación.",
+        "Honorarios regulados judicialmente.",
+      ],
+    },
+    {
+      title: "+ Familia",
+      content: [
+        "Divorcios unilaterales y bilaterales.",
+        "Acuerdos de distribución de bienes.",
+        "Uniones convivenciales.",
+        "Reclamos patrimoniales del cese de la unión.",
+        "Responsabilidad parental integral.",
+        "Reclamos de filiación.",
+        "Régimen de comunicación.",
+        "Cuotas alimentarias.",
+        "Impedimento de contacto.",
+        "Reclamo de prestación alimentaria e incidentes de aumento de prestaciones alimentarias.",
+        "Responsabilidad subsidiaria y complementaria de abuelos.",
+      ],
+    }, // No tiene contenido
+    {
+      title: "+ Laboral",
+      content: [
+        "Te acompañamos en despidos sin causa.",
+        "Indemnizaciones por despido.",
+        "Despido discriminatorio.",
+        "Demandas contra Aseguradoras de Riego del Trabajo por accidentes de trabajo y enfermedades laborales.",
+        "Maltrato laboral.",
+        "Accidentes laborales.",
+        "Trabajo no registrado.",
+        "Diferencia salarial.",
+        "Trámites ante la Comisión Médica de la Superintendencia de Trabajo de la Nación.",
+        "Reincorporación al puesto de trabajo.",
+        "Despido indirecto.",
+      ],
+    },
+    { title: "+ Reales", content: [] },
+    { title: "+ Sucesiones", content: [] },
+    { title: "+ Violencia de género", content: [] },
+  ];
+
   return (
-    <div className="bg-white text-black">
+    <div>
       <Navbar />
-      <h1 className="text-center m-20 p-8">SERVICIOS</h1>
-      <p className="px-64 ">
-        Ofrecemos un trato personalizado para atender las necesidades de
-        nuestros clientes. Ponemos a disposición entrevistas presenciales y
-        virtuales, asesorías online pre y post sentencia, y mediaciones.
-        <br />
-        Nuestro valor es garantizar la pronta respuesta a tus necesidades desde
-        un enfoque integrado, creativo y con el compromiso para lograr la
-        efectividad de nuestros servicios. Mantenerte informado sobre la
-        evolución de tu caso es nuestra prioridad.
-        <br />
-        Dale un vistazo a nuestras áreas de práctica para ayudarte a resolver
-        tus problemas.
-      </p>
-      <ol className="px-64 py-8">
-        <li className="p-2">Administrativo</li>
-        <li className="p-2">Civil</li>
-        <li className="p-2">Familia</li>
-        <li className="p-2">Laboral</li>
-        <li className="p-2">Reales</li>
-        <li className="p-2">Suseciones</li>
-        <li className="p-2">Violencia de genero</li>
-      </ol>
+      <div className="bg-white text-black">
+        <div className="bg-[url('/imagenes/fontpage.jpg')] bg-cover bg-center h-full w-full my-20 py-44">
+          <h1 className="text-4xl text-white text-center">Servicios</h1>
+        </div>
+        <p className="px-44 ">
+          Ofrecemos un trato personalizado para atender las necesidades de
+          nuestros clientes. Ponemos a disposición entrevistas presenciales y
+          virtuales, asesorías online pre y post sentencia, y mediaciones.
+          <br />
+          Nuestro valor es garantizar la pronta respuesta a tus necesidades
+          desde un enfoque integrado, creativo y con el compromiso para lograr
+          la efectividad de nuestros servicios. Mantenerte informado sobre la
+          evolución de tu caso es nuestra prioridad.
+          <br />
+          Dale un vistazo a nuestras áreas de práctica para ayudarte a resolver
+          tus problemas.
+        </p>
+        <ol className="px-64 py-8">
+      {sections.map((section, index) => (
+        <li
+          key={index}
+          className="bg-gray-400 hover:bg-gray-900 hover:text-white cursor-pointer p-2"
+          onClick={() => toggleList(index)} // Cambia la lista activa
+        >
+          {section.title}
+          {/* Si la lista está activa, se muestra el contenido */}
+          {section.content.length > 0 && (
+            <ol className={`${activeIndex === index ? 'block' : 'hidden'} ml-4 mt-2 list-disc`}>
+              {section.content.map((item, i) => (
+                <li key={i}>{item}</li>
+              ))}
+            </ol>
+          )}
+        </li>
+      ))}
+    </ol>
+      </div>
     </div>
   );
 }
