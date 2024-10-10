db811db9ac6f5338127162c141d7636b3d942ad9
Facundo Sura <facundomesura@gmail.com>
2024-10-10 05:54:04 -0300

    landing bienvenida lista

--- client/src/app/inicio/page.tsx
+++ client/src/app/inicio/page.tsx
@@ -38,28 +38,36 @@ export default function Inicio() {
       {/* Contenido debajo del video */}
       <div className="relative z-10 p-8">
         <div>
-          <p className="text-center text-base font-light m-8 p-20">
-            Bienvenidos a Estudio Juridico Sura, donde la experiencia y la
-            excelencia se unen para brindar soluciones integrales en el ámbito
-            legal. Somos un grupo de profesionales altamente capacitados,
-            compuesto por expertos en diversas áreas del derecho.
-          </p>
-          <h2 className="text-center font-bold text-2xl font-sans m-2 p-2">
-            ASESORAMIENTO PROFESIONAL
-          </h2>
-          <p className="text-center text-base font-light mx-8 py-4 p-20">
-            Comprendemos que cada situación legal es única, y trabajamos en
-            estrecha colaboración con nuestros clientes para encontrar la mejor
-            solución posible, siempre con el mayor cuidado y atención. Nuestra
-            filosofía nos ha permitido ganar una reputación sólida como uno de
-            los estudios de abogados más confiables y efectivos en nuestra área
-            de práctica. Nos encantaría poder ayudarte en cualquier situación
-            legal que puedas enfrentar. Agendá una cita con nosotros
-          </p>
+          <div className="flex justify-center items-center bg-gray-100 rounded-2xl p-20">
+            <p className="text-center text-base font-light m-8 p-10 w-1/2">
+              Bienvenidos a Estudio Juridico Sura, donde la experiencia y la
+              excelencia se unen para brindar soluciones integrales en el ámbito
+              legal. Somos un grupo de profesionales altamente capacitados,
+              compuesto por expertos en diversas áreas del derecho.
+            </p>
+            <video autoPlay loop muted className="h-80 rounded-2xl">
+              <source src="/videos/bienvenido.mp4" />
+            </video>
+          </div>
+          <div className="h-80">
+            <h2 className="text-center font-bold text-2xl font-sans m-4 p-10">
+              ASESORAMIENTO PROFESIONAL
+            </h2>
+            <p className="text-center text-base font-light mx-8 py-4 p-20">
+              Comprendemos que cada situación legal es única, y trabajamos en
+              estrecha colaboración con nuestros clientes para encontrar la
+              mejor solución posible, siempre con el mayor cuidado y atención.
+              Nuestra filosofía nos ha permitido ganar una reputación sólida
+              como uno de los estudios de abogados más confiables y efectivos en
+              nuestra área de práctica. Nos encantaría poder ayudarte en
+              cualquier situación legal que puedas enfrentar. Agendá una cita
+              con nosotros
+            </p>
+          </div>
           <div className="my-4 p-1 text-center">
             <Link
               href="/nosotros"
-              className="border-2 border-solid border-black p-5 hover:translate-x-6 hover:bg-slate-400 hover:bg-opacity-20 hover:underline"
+              className="relative border-2 border-solid border-black p-5 hover:translate-x-6 hover:bg-slate-400 hover:bg-opacity-30 transition-all duration-300 ease-in-out before:content-[''] before:absolute before:bottom-0 before:left-0 before:w-full before:h-[2px] before:bg-black before:scale-x-0 hover:before:scale-x-100 before:origin-left before:transition-transform before:duration-1000"
             >
               Saber mas
             </Link>
@@ -68,8 +76,8 @@ export default function Inicio() {
             ¿En qué podemos ayudarte?
           </h2>
           <div className="grid grid-cols-2 gap-4 justify-items-center">
-          <div className="bg-[url('/imagenes/da.jpg')] bg-cover bg-center text-white text-center p-20 w-2/3 h-64 relative overflow-hidden hover:p-5 duration-1000 group">
-          <h3 className="bg-black bg-opacity-50 border-2 py-4 text-4xl relative  group-hover:opacity-0 transition-opacity duration-1000">
+            <div className="bg-[url('/imagenes/da.jpg')] bg-cover bg-center text-white text-center p-20 w-2/3 h-64 relative overflow-hidden hover:p-5 duration-1000 group">
+              <h3 className="bg-black bg-opacity-50 border-2 py-4 text-4xl relative  group-hover:opacity-0 transition-opacity duration-1000">
                 Administrativo
               </h3>
               <div className="absolute inset-0 bg-black bg-opacity-50 m-2 flex flex-col items-center justify-center transition-all duration-1000 opacity-0 translate-y-full group-hover:translate-y-0 group-hover:opacity-100 z-10">
@@ -82,7 +90,7 @@ export default function Inicio() {
                 <div className="p-5 text-center">
                   <Link
                     href="/servicios"
-                    className="border-2 border-solid border-white p-3 hover:bg-slate-400 hover:bg-opacity-30"
+                    className="border-2 border-solid border-white p-3 transition-all hover:bg-slate-400  hover:bg-opacity-30 duration-1000"
                   >
                     Saber mas
                   </Link>
@@ -102,7 +110,7 @@ export default function Inicio() {
                 <div className="p-5 text-center">
                   <Link
                     href="/servicios"
-                    className="border-2 border-solid border-white p-3 hover:bg-slate-400 hover:bg-opacity-30"
+                    className="border-2 border-solid border-white p-3 transition-all hover:bg-slate-400  hover:bg-opacity-30 duration-1000"
                   >
                     Saber mas
                   </Link>
@@ -122,7 +130,7 @@ export default function Inicio() {
                 <div className="p-5 text-center">
                   <Link
                     href="/servicios"
-                    className="border-2 border-solid border-white p-3 hover:bg-slate-400 hover:bg-opacity-30"
+                    className="border-2 border-solid border-white p-3 transition-all hover:bg-slate-400  hover:bg-opacity-30 duration-1000"
                   >
                     Saber mas
                   </Link>
@@ -143,7 +151,7 @@ export default function Inicio() {
                 <div className="p-5 text-center">
                   <Link
                     href="/servicios"
-                    className="border-2 border-solid border-white p-3 hover:bg-slate-400 hover:bg-opacity-30"
+                    className="border-2 border-solid border-white p-3 transition-all hover:bg-slate-400  hover:bg-opacity-30 duration-1000"
                   >
                     Saber mas
                   </Link>
@@ -166,7 +174,7 @@ export default function Inicio() {
                 <div className="p-5 text-center">
                   <Link
                     href="/servicios"
-                    className="border-2 border-solid border-white p-3 hover:bg-slate-400 hover:bg-opacity-30"
+                    className="border-2 border-solid border-white p-3 transition-all hover:bg-slate-400  hover:bg-opacity-30 duration-1000"
                   >
                     Saber mas
                   </Link>
@@ -187,7 +195,7 @@ export default function Inicio() {
                 <div className="p-5 text-center">
                   <Link
                     href="/servicios"
-                    className="border-2 border-solid border-white p-3 hover:bg-slate-400 hover:bg-opacity-30"
+                    className="border-2 border-solid border-white p-3 transition-all hover:bg-slate-400  hover:bg-opacity-30 duration-1000"
                   >
                     Saber mas
                   </Link>
